7b10ee27a3508e33f00d1581389f12d0a919a0f9
Jakub Korytko <jakub@korytko.me>
2025-05-12 21:38:13 +0200

    Adjust template for new logic

--- templates/exampleApp/src/modules/consoleTree.pig.mjs
+++
@@ -1,9 +0,0 @@
-const consoleTree = () => {
-  console.groupCollapsed("🌲 Component's tree:");
-  console.log(window.Piglet.tree);
-  console.dir(window.Piglet.tree);
-  console.groupEnd();
-};
-
-// noinspection JSUnusedGlobalSymbols
-export default consoleTree;
